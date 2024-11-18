973eef348f14c0523a20491f5f187bc0b5e8161e
anish102 <anishkarki695@gmail.com>
2024-11-18 22:00:43 +0545

    First Commit

---
+++ db.sql
@@ -0,0 +1,20 @@
+--Create database
+CREATE DATABASE sqllearn;
+
+--Delete database
+DROP DATABASE sqllearn;
+
+--Create table with column name
+CREATE TABLE COMPANY(
+    ID INT PRIMARY KEY NOT NULL,
+    NAME TEXT NOT NULL,
+    AGE INT NOT NULL,
+    ADDRESS CHAR(50),
+    SALARY REAL
+);
+
+CREATE TABLE DEPARTMENT(
+    ID INT PRIMARY KEY NOT NULL,
+    DEPT CHAR(50) NOT NULL,
+    EMP_ID INT NOT NULL
+);
---
+++ readme.md
@@ -0,0 +1,3 @@
+Welcome to the SQL Learning Repository! 
+This repository contains a collection of SQL exercises, examples, and tutorials designed to help me learn and practice SQL.
+I use PostgreSQL for my learnings.
